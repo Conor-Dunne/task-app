0f77cc77184850e1be77ba09b091dd42e67f9f6e
Conor Dunne <conordunne@outlook.ie>
2022-12-15 06:14:21 +0900

    Add task handlers and Overview.js

--- src/App.js
+++ src/App.js
@@ -12,14 +12,30 @@ class App extends Component {
 
   }
 
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {text: ''},
+    })
+  }
+
   render() {
     const { task, tasks } = this.state;
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmitTask}>
           <label htmlFor='taskInput'>Enter task</label>
-          <input type='text' id='taskInput' />
+          <input onChange={this.handleChange} value={task.text} type='text' id='taskInput' />
           <button type='submit'>
             Add Task
           </button>
